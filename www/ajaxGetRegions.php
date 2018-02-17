d6be51ed59dd31ee36e1204164183dc772c09a54
raoyan18 <r.aoyan18@gmail.com>
2018-02-17 13:44:08 +0600

    load country from database

--- www/ajaxGetRegions.php
+++ www/ajaxGetRegions.php
@@ -4,18 +4,18 @@
     include ROOT.'/www/ctrl.php';
     $states = Country::getStates($_GET['country']);
     
-    if ($_GET['json']) {
-        echo json_encode($states);
-        exit;
-    }
-    
-    if (!count($states)) {
-        exit;
-    }
+//    if ($_GET['json']) {
+//        echo json_encode($states);
+//        exit;
+//    }
+//
+//    if (!count($states)) {
+//        exit;
+//    }
 ?>
 <select name="state">
 <option>Not selected</option>
-<?php foreach($states as $state): ?>
-<option value="<?php echo $state ?>"><?php echo $state ?></option>
+<?php foreach($states as $key => $state): ?>
+    <option value="<?php echo $state['name']?>"><?php echo $state['name']?></option>
 <?php endforeach; ?>
 </select>
